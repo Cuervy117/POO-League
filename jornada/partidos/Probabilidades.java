538ee1f64e98b18b259d5fb8e459bf533ba927b7
Diaz David <davidadiaz117@gmail.com>
2024-09-19 10:33:48 -0600

    Primera propuesta de cotas

--- jornada/partidos/Probabilidades.java
+++ jornada/partidos/Probabilidades.java
@@ -5,8 +5,14 @@ public class Probabilidades {
     private double cotaInferior;
     private double cotaSuperior;
 
-    public Probabilidades(int partidosGanados, int partidosPerdidos, int Empates){
-        
+    public Probabilidades(int partidosGanados, int partidosPerdidos, int empates){
+        cotaInferior =2 * Math.log(partidosPerdidos + 1)*Math.exp(-partidosPerdidos);
+        cotaSuperior = Math.log(partidosGanados + 1) + 1;
+    }
+
+    public void imprimirCotas(){
+        System.out.println(cotaInferior);
+        System.out.println(cotaSuperior);
     }
 
 }
