03b1741300066b39fa7f9172b14d886128e804b4
Diaz David <davidadiaz117@gmail.com>
2024-09-19 09:25:29 -0600

    Comenzando la caracteristicas

---
+++ jornada/partidos/Probabilidades.java
@@ -0,0 +1,12 @@
+package jornada.partidos;
+import java.util.Random;
+
+public class Probabilidades {
+    private double cotaInferior;
+    private double cotaSuperior;
+
+    public Probabilidades(int partidosGanados, int partidosPerdidos, int Empates){
+        
+    }
+
+}
