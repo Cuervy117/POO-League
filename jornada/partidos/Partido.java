05cb0d76135115433b060b68c0b537e998919192
David Díaz <143227166+Cuervy117@users.noreply.github.com>
2024-09-21 16:50:40 -0600

    Clase Partido sin metodos de clase

--- jornada/partidos/Partido.java
+++ jornada/partidos/Partido.java
@@ -1,14 +1,31 @@
 package jornada.partidos;
 import equipos.Equipo;
 public class Partido{
-    private Equipo visitante;
-    private Equipo local;
+    private Equipo local, visitante;
+    private int golesLocal, golesVisitante;
     
-    public void simularPartido(){
-        System.out.println("Simulando partido...");
+    public Partido(Equipo local, Equipo visitante){
+        this.local = local;
+        this.visitante = visitante;
     }
 
-    public void mostrarMarcador(){
-        System.out.println("Mostrando marcador...");
+    // region getters
+    public Equipo getLocal(){
+        return local;
     }
+
+    public Equipo getVisitante(){
+        return visitante;
+    }
+
+    public int getGolesLocal(){
+        return golesLocal;
+    }
+
+    public int getGolesVisitante(){
+        return golesVisitante;
+    }
+    // endregion
+
+    
 }
