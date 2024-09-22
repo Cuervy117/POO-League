87224bf2a7e566279d46b0a01d03a29cdf48cad6
Samuel Santiago Cruz Macedo <santi.sam120@gmail.com>
2024-09-21 19:28:21 -0600

    Borré partidos

---
+++ registrar/Partido.java
@@ -0,0 +1,9 @@
+public class Partido {
+    int minutos = 90;
+    Equipo local;
+    Equipo visitante;
+    public Partido(Equipo local, Equipo visitante){
+        this.local = local;
+        this.visitante = visitante;
+    }
+}
