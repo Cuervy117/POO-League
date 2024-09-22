8aabf606c0e9d07581631e295cf4091c9a352090
David Díaz <143227166+Cuervy117@users.noreply.github.com>
2024-09-22 15:10:08 -0600

    Documentacion de la clase ProbaGoles

--- probabilidades/ProbaGoles.java
+++ probabilidades/ProbaGoles.java
@@ -6,13 +6,13 @@ import java.lang.Math;
  */
 public class ProbaGoles{
     // Atributos 
-    private float golesMin;
-    private float golesMax;
+    private float golesMax; // Cota superior
+    private float golesMin; // Cota inferior 
 
     /**
      * Constructor para la clase ProbaGoles
-     * @param puntos 
-     * @param totalPartidos
+     * @param puntos Los puntos del equipo hasta el momento
+     * @param totalPartidos Los partidos jugados por el equipo hasta el momneto  
      */
     public ProbaGoles(int puntos, int totalPartidos){
         int partidosAFavor = puntos/3;
@@ -20,11 +20,20 @@ public class ProbaGoles{
         golesMin = (float)(Math.log(Math.pow(golesMax , 4) + 1) / 4);
     }
 
+    /**
+     * Metodo para actuazliar las cotas 
+     * @param puntos Los puntos del equipo hasta el momento
+     * @param totalPartidos Los partidos jugados por el equipo hasta el moment
+     */
     public void updateGoles(int puntos, int totalPartidos){
         golesMax = (float)(Math.log((puntos/(totalPartidos + 1.0)) + 1) + 1);
         golesMin = (float)(Math.log(Math.pow(golesMax , 4) + 1) / 4);
     }
 
+    /**
+     * Metodo que calcula los goles dependiendo de las cotas
+     * @return Devuelve un número de goles
+     */
     public int getGoles(){
         Random rnd = new Random();
         return Math.round(rnd.nextFloat() * (golesMax - golesMin) + golesMin);
