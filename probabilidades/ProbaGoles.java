0a0bd57f0a69d374e2cc3e443474dcc3db606488
David Díaz <143227166+Cuervy117@users.noreply.github.com>
2024-09-21 21:51:49 -0600

    probabilidades de gol adaptadas a puntos y partidos totales

--- probabilidades/ProbaGoles.java
+++ probabilidades/ProbaGoles.java
@@ -6,27 +6,27 @@ import java.lang.Math;
  */
 public class ProbaGoles {
     // Atributos 
-    private double golesMin;
-    private double golesMax;
+    private float golesMin;
+    private float golesMax;
 
     /**
      * Constructor para la clase ProbaGoles
-     * @param partidosGanados 
-     * @param partidosPerdidos
+     * @param puntos 
+     * @param totalPartidos
      */
-    public ProbaGoles(int partidosGanados, int partidosPerdidos){
-        golesMax = Math.log((partidosGanados/(partidosPerdidos + 1)) + 1) + 1;
-        golesMin = Math.log(Math.pow(golesMax , 4) + 1) / 4;
-    }
-
-    public void updateProba(int partidosGanados, int partidosPerdidos){
-        golesMax = Math.log((partidosGanados/(partidosPerdidos + 1)) + 1) + 1;
-        golesMin = Math.log(Math.pow(golesMax , 4) + 1) / 4;
+    public ProbaGoles(int puntos, int totalPartidos){
+        int partidosAFavor = (puntos) / 3;
+        golesMax = (float)(Math.log((partidosAFavor / (totalPartidos - partidosAFavor + 1))) + 1);
+        golesMin = (float)(Math.log(Math.pow(golesMax , 4) + 1) / 4);
     }
     
+    public void updateGoles(int puntos, int totalPartidos){
+        golesMax = (float)(Math.log((puntos/(totalPartidos + 1.0)) + 1) + 1);
+        golesMin = (float)(Math.log(Math.pow(golesMax , 4) + 1) / 4);
+    }
     public long goles(){
         Random rnd = new Random();
-        return Math.round(rnd.nextDouble(golesMax - golesMin) + golesMin);
+        return Math.round(rnd.nextFloat() * (golesMax - golesMin) + golesMin);
     }
 
 }
